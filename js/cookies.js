2724fb81fe1d2ad4999c163870257098c82ba35c
zakochaniwhiszpanii <zakochaniwhiszpanii@gmail.com>
2021-05-19 22:27:02 +0200

    20210519-2227

---
+++ js/cookies.js
@@ -0,0 +1,49 @@
+// Creare's 'Implied Consent' EU Cookie Law Banner v:2.4
+// Conceived by Robert Kent, James Bavington & Tom Foyster
+
+var dropCookie = true;                      // false disables the Cookie, allowing you to style the banner
+var cookieDuration = 14;                    // Number of days before the cookie expires, and the banner reappears
+var cookieName = 'complianceCookie';        // Name of our cookie
+var cookieValue = 'on';                     // Value of cookie
+
+function createCookie(name, value, days) {
+    if (days) {
+        var date = new Date();
+        date.setTime(date.getTime() + (days * 24 * 60 * 60 * 1000));
+        var expires = "; expires=" + date.toGMTString();
+    }
+    else {
+        var expires = "";
+    }
+    document.cookie = name + "=" + value + expires + "; path=/";
+}
+
+function getCookieValue(name) {
+    var nameEQ = name + "=";
+    var ca = document.cookie.split(';');
+    for (var i = 0; i < ca.length; i++) {
+        var c = ca[i];
+        while (c.charAt(0) == ' ') c = c.substring(1, c.length);
+        if (c.indexOf(nameEQ) == 0) return c.substring(nameEQ.length, c.length);
+    }
+    return null;
+}
+
+function eraseCookie(name) {
+    createCookie(name, "", -1);
+}
+
+function storeConsentCookie() {
+    createCookie(window.cookieName, window.cookieValue, window.cookieDuration);
+}
+
+function showCookieAlert() {
+    var el = document.getElementById('cookie-alert')
+    el.removeAttribute("style")
+}
+
+window.onload = function () {
+    if (getCookieValue(window.cookieName) != window.cookieValue) {
+        showCookieAlert();
+    }
+}
