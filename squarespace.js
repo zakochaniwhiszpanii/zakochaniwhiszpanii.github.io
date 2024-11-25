f26557f25d101f28a7c347563e7893ef5b0ec728
zakochaniwhiszpanii <73141240+zakochaniwhiszpanii@users.noreply.github.com>
2024-11-25 10:02:07 +0100

    hide foreign nav fn

--- squarespace.js
+++ squarespace.js
@@ -1,1 +1,30 @@
-console.log('hello from gist');
+function getLanguage() {
+    return localStorage.getItem('language') || 'pl';
+}
+
+function hideForeignNavigation() {
+    const lang = getLanguage();
+    const links = document.querySelectorAll('.header-nav-item > a:not([href^="/' + lang + '"])');
+    links.forEach(function (it) {
+        if (!it.href.match('.*/search/.*')) {
+            console.log('hideForeignNavigation hiding', it.href);
+            it.style.display = 'none';
+        }
+    });
+}
+
+function translateComments() {
+    const commentsAll = document.querySelectorAll("#comments");
+    commentsAll.forEach(function (comments) {
+        const cm = comments.querySelector("h3 span");
+        cm.innerText = cm.innerText.replaceAll("Comments", "Komentarze");
+        comments.querySelector(".comment-btn").innerText = "Skomentuj";
+        comments.querySelector(".preview-comment").innerText = "Podgląd";
+    });
+}
+
+
+document.addEventListener('readystatechange', function () {
+    hideForeignNavigation();
+    translateComments();
+})
